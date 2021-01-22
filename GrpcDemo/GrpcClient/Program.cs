d48d618d91128d69bd5b9799f75e79b5d4327566
Mert <mert.duatepe@gmail.com>
2021-01-22 22:50:43 +0000

    add grpc and asp dotnet core implementation with grpc service and grpc client
    it includes following examples:
    * unary grpc call
    * client streaming
    * server streaming
    * bidirectional grpc call

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine("Sending unary call...");
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine("Unary Response: " + reply.Message);
+
+            Console.WriteLine("Sending request for server stream...");
+            using (var call = client.SayManyHellos(new HelloRequest { Name = "GreeterClient"}))
+            {
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("New element from response stream: " + response.Message);
+                }
+            }
+
+            Console.WriteLine("Sending client stream...");
+            var listOfNames = new List<String> { "John", "James", "Freddy", "David" };
+
+            Console.WriteLine("Names about to be sent: " + string.Join(", ", listOfNames));
+            using (var call = client.SayHelloToLastRequest())
+            {
+                foreach (var name in listOfNames)
+                {
+                    await call.RequestStream.WriteAsync(new HelloRequest { Name = name });
+                }
+
+                await call.RequestStream.CompleteAsync();
+
+                Console.WriteLine("Response from client stream: " + (await call.ResponseAsync).Message);
+            }
+
+            Console.WriteLine("Sending bi-directional stream...");
+            using(var call = client.SayHelloToEveryRequest())
+            {
+                foreach (var name in listOfNames)
+                {
+                    await call.RequestStream.WriteAsync(new HelloRequest { Name = name });
+                }
+
+                await call.RequestStream.CompleteAsync();
+
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("Individual item from bi-directional call: " + response.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
